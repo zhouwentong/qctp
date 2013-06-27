579e512118b4542e024ca35105e3928340b9e7bf
zhouwentong <vintone1984@gmail.com>
2013-06-27 09:58:41 +0800

    更改m6中li

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(function(){
 		$('.m11 .l2 img').attr('src',src);
 		$('.m11 .l2 h3').text(title);
 	})
-	$('.m6 li:odd').css('background',"#E9E9E9");
+	$('.m6 li:nth-child(4n+3)').css('background',"#E9E9E9");
+	$('.m6 li:nth-child(4n+4)').css('background',"#E9E9E9");
 	$('.ma .br2 li:last-child').css('margin-left','10px');
 });
